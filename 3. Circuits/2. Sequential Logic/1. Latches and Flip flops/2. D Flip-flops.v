1a1b2fe5c7d11d513030e8b224c6fb7233a1f81a
Sreekar Mallela <sreekarmallela2512@gmail.com>
2026-09-14 17:31:07 +0000

    Latches: Update

---
+++ 3. Circuits/2. Sequential Logic/1. Latches and Flip flops/2. D Flip-flops.v
@@ -0,0 +1,12 @@
+module top_module (
+    input clk,
+    input [7:0] d,
+    output [7:0] q
+);
+    integer i;
+    always @ (posedge clk)begin 
+        for(i=0;i<8;i=i+1)begin 
+        q<=d;
+        end
+    end
+endmodule
